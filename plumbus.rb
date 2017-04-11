f09853ec25e1d815b33bf16ffd09fc019510a071
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-04-11 13:56:42 -0400

    first round of styling

--- plumbus.rb
+++ plumbus.rb
@@ -2,9 +2,7 @@ require "sinatra"
 
 get "/" do
   erb :home
-  "This is working"
 end
-# folder that this will be found in has to be - views
 get "/contact" do
   erb :contact
 end
---
+++ public/plumbus.js
@@ -0,0 +1,4 @@
+var audio = document.getElementById("opening");
+var floobBtn =doucment.getElementById("floob");
+var grodusBtn =document.getElementById("grodus");
+var 
